1578202ec6bb7bf99c69765ba5c54a2375956fd5
effilin <effienummer@gmail.com>
2024-08-30 20:40:02 -0400

    delete and update functions working

--- capstone-puzzle-app/src/app/api/users/route.js
+++ capstone-puzzle-app/src/app/api/users/route.js
@@ -51,6 +51,55 @@ export async function GET(request) {
             return new Response(JSON.stringify({error: 'error retrieving '+ error}))
         }
 };
+
+export async function DELETE(request) {
+    
+    try {
+        console.log(request.nextUrl.searchParams)
+        const searchParams = request.nextUrl.searchParams
+       
+        const name= searchParams.get('name')
+        const zipCode = searchParams.get('zipCode')
+        console.log( `decompose searchParams.get('name'): ${name}, decompose request.nextUrl.searchParams.get('zipcode'): ${zipCode}`)
+
+        if(!name || !zipCode) {
+            return new Response(JSON.stringify({error: 'name or zip code not received'}))
+        };
+        await User.deleteOne({ name: name, zipCode: zipCode })
+
+         return new Response(JSON.stringify('deleted'), {status: 200})
+    } catch (error) {
+            return new Response(JSON.stringify({error: 'error retrieving '+ error}))
+        }
+};
+
+export async function PUT(req, res) {
+    
+    try {
+        console.log(req.nextUrl.searchParams)
+        const searchParams = req.nextUrl.searchParams
+       
+        const newName= searchParams.get('name')
+        const newZipCode = searchParams.get('zipCode')
+        const newTheme = searchParams.get('theme')
+        
+        
+        const { name, zipCode, theme} = await req.json()
+    
+        const updatedUser = await User.updateOne({name: name, zipCode: zipCode },
+            {$set: {name: newName, zipCode: newZipCode, theme: newTheme}});
+
+        console.log(updatedUser)
+        return new Response(JSON.stringify({message:'User updated successfully!'}),
+         {status: 201,
+          headers:{'Content-Type': 'application/json'}
+          })
+    
+        } catch (error){
+            return new Response(JSON.stringify({ error: 'Error creating user' }), {
+                status: 500,
+        })}
+    }
      
    
     
